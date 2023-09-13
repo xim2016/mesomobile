7b1a66dcc27f5068ced42069175d6d7abce16238
xim2016 <xj_ma@yahoo.com>
2023-09-13 16:47:18 -0400

    updates

--- utils_mobile.py
+++ utils_mobile.py
@@ -214,12 +214,13 @@ def show_plotly_image(img_file, height=750):
               'toImageButtonOptions': { 'height': None, 'width': None, 
                                        'filename': 'core_img',},
                'modeBarButtonsToRemove': ['zoom', 'resetScale'],
-               'modeBarButtonsToAdd': ['eraseshape',
+               'modeBarButtonsToAdd': ['pan'
                                        'drawline',
                                         'drawopenpath',
                                         'drawclosedpath',
                                         'drawcircle',
-                                        'drawrect'
+                                        'drawrect',
+                                        'eraseshape'
                                        ]}
     st.plotly_chart(fig, use_container_width=True, height=height, config=config)
     
