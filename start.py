071e56eb0da4c84845ccfc88fd5e0d735418be90
xim2016 <xj_ma@yahoo.com>
2023-09-13 15:29:25 -0400

    updates

--- start.py
+++ start.py
@@ -67,8 +67,8 @@ chosen_tab = option_menu(None, ["About", "Data",  "Contact", "Citation"],
     menu_icon="cast", default_index=0, orientation="horizontal",
     styles={
         "container": {"padding": "0!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "25px"}, 
-        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "icon": {"color": "orange", "font-size": "10px"}, 
+        "nav-link": {"font-size": "10px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
         "nav-link-selected": {"background-color": "#0f4d92"},
     }
 )
