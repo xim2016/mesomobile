8ccfd08d8500f3e3dd8503a2fa5438c3ea44a9d9
xim2016 <xj_ma@yahoo.com>
2023-09-13 20:28:37 -0400

    updates

--- style.py
+++ style.py
@@ -5,7 +5,7 @@ footer="""
   
     .image 
     { 
-        padding: 10px;
+        padding: 5px;
         transition: transform .2s;
     }
 
@@ -29,7 +29,8 @@ footer="""
 </style>
 
 <div class="footer">
-<p style="font-size:  13px">© 2023 Osmanbeyoglulab.com. All rights reserved.</p>
+<p style="font-size:  13px">Osmanbeyoglulab.com </p>
+<p style="font-size:  13px">© 2023 All rights reserved. </p>
 <a href="https://hillman.upmc.com/"><img class="image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7c7pXIkFgMPVM2csVE6MenUFLEsgF5beCeMJzogkPkXPC4xEo9OTHf6nVpqsb3PvisRk&usqp=CAU"alt="github" width="70" height=50"></a>
 <a href="https://www.pitt.edu/"><img class="image" src="https://upload.wikimedia.org/wikipedia/en/thumb/f/fb/University_of_Pittsburgh_seal.svg/300px-University_of_Pittsburgh_seal.svg.png"alt="github" width="45" height="45"></a>
 <a href="https://github.com/osmanbeyoglulab"><img class="image" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github" width="45" height="45"></a>
